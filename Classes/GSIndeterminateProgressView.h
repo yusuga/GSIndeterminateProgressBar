51c819e201e8f32b915355cb04ff10a111c6175a
Yu Sugawara <yusuga@me.com>
2014-07-31 04:44:05 +0900

    add appearance

--- Classes/GSIndeterminateProgressView.h
+++ Classes/GSIndeterminateProgressView.h
@@ -12,8 +12,8 @@
 
 @property (nonatomic, strong) NSArray   * progressChunks;
 
-@property (nonatomic, strong) UIColor   * progressTintColor;
-@property (nonatomic, strong) UIColor   * trackTintColor;
+@property (nonatomic, strong) UIColor   * progressTintColor UI_APPEARANCE_SELECTOR;
+@property (nonatomic, strong) UIColor   * trackTintColor UI_APPEARANCE_SELECTOR;
 
 /*! A Boolean value that controls whether the receiver is hidden when the animation is stopped. Default YES */
 @property (nonatomic) BOOL hidesWhenStopped;
